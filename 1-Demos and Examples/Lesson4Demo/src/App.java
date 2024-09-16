11fd2d778afe4760068d39538e82dfe678e23d78
f7ate <faith.uwaechi@rcsd.ca>
2024-09-16 08:54:15 -0600

    Working on A4

---
+++ 1-Demos and Examples/Lesson4Demo/src/App.java
@@ -0,0 +1,14 @@
+// Project Name: Lesson 4 Demo
+// Name: FAITH
+// Date: Sept 13th 2024
+// Description: Going over comments and special characters
+/*yes */
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("How is your day going");
+        System.out.println("It's cloudy with a chance of meatballs");
+    }
+}
---
+++ A4 ASCII Art/src/AsciiArt.java
@@ -0,0 +1,30 @@
+/* Project Name: Ascii Art
+ Name: FAITH
+ Date: Sept 13th 2024
+ Description: Ascii Animal Art 
+ */
+
+
+public class AsciiArt 
+{
+    public static void main ( String[] args )
+    {
+    
+	{
+		System.out.println("Your Name \t  Date \t Period\n\n" );
+		System.out.println("What type of ANIMAL YOU WILL DRAW" );
+		System.out.println("\n\n\n\n" );
+
+
+		System.out.println("                /\\                " );
+		System.out.println("               /  \\               " );
+		System.out.println("              /    \\              " );
+		System.out.println("             [------]             " );
+		//add other output
+
+		System.out.println(" \n\n\n\nHelpFul Hints" );
+		System.out.println("\\\\ draws one backslash on the screen!\n" );
+		System.out.println("\\\" draws one double quote on the screen!\n" );
+		System.out.println("\\\' draws one single quote on the screen!\n" ); 
+    }
+}

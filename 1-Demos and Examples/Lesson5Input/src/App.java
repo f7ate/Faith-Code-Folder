629c79ea6412631fbf7d4d86c3845da7d6abb716
f7ate <faith.uwaechi@rcsd.ca>
2024-09-23 09:48:54 -0600

    monday

---
+++ 1-Demos and Examples/Lesson5Input/src/App.java
@@ -0,0 +1,18 @@
+//
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner keys = new Scanner(System.in);
+
+        System.out.println("Enter an integer: ");
+        int num = keys.nextInt();
+        keys.nextLine();
+        System.out.println("You selected the number" + num);
+
+        System.out.print("What is your name? ");
+        String name = keys.nextLine();
+        System.out.println("You entered: " + name);
+
+    }
+}

4a2c3c9444d07ba8f629d9d7c2ec9944e1ca52cf
f7ate <faith.uwaechi@rcsd.ca>
2024-09-27 09:49:07 -0600

    tuesday

---
+++ 1-Demos and Examples/Lesson6MethosDemo/src/Turkey.java
@@ -0,0 +1,11 @@
+public class Turkey {
+    public void speak() {
+        System.out.println("Gobble-Gobble");
+    }
+
+    public void sayName() {
+        System.out.println("Big Bird");
+        speak();
+    }
+
+}
--- 1-Demos and Examples/LessonVariables/src/App.java
+++ 1-Demos and Examples/LessonVariables/src/App.java
@@ -1,3 +1,4 @@
+
 public class App {
     public static void main(String[] args) throws Exception {
         //System.out.println(1 / 3);

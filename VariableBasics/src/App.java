6c46ec242c502585ef916cf7c365b31b70350d0b
f7ate <faith.uwaechi@rcsd.ca>
2024-10-03 09:48:38 -0600

    A6

--- VariableBasics/src/App.java
+++ VariableBasics/src/App.java
@@ -1,5 +1,12 @@
-public class App {
+/* Project Name: Variable Basics
+ Name: FAITH
+ Date: Oct 3rd 2024
+ Description: Defineing and outputing a variable.
+ */
+
+ public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double wingCost = 0.75;
+        double Cost = 4.75;
     }
 }

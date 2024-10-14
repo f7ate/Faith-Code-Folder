7160b155179ebca6c8335f5eeb881926ecda8a53
f7ate <faith.uwaechi@rcsd.ca>
2024-10-14 13:36:34 -0600

    A7 done

--- VariableBasics/src/App.java
+++ VariableBasics/src/App.java
@@ -1,12 +1,18 @@
-/* Project Name: Variable Basics
+/*Project Name: Variable Basics - Chicken Wing
  Name: FAITH
- Date: Oct 3rd 2024
- Description: Defineing and outputing a variable.
- */
+ Date: Oct 11th 2024
+ Description: Defining and outputing a variable.
+*/
 
- public class App {
+public class App {
     public static void main(String[] args) throws Exception {
-        double wingCost = 0.75;
-        double Cost = 4.75;
+        double wingCost = 1.50;
+        double totalMoney = 15.75;
+        /*assigning variables */
+
+        double getWings = (totalMoney/wingCost);
+        System.out.println("The total amount of wings you can get is " + getWings);
+        /*output */
+        
     }
 }

85fc86da5adac0c19dce3a3d4a800606000892c2
f7ate <faith.uwaechi@rcsd.ca>
2024-10-30 09:48:26 -0600

    oct 30th

---
+++ A9 Big or Small/src/BigOrSmall.java
@@ -0,0 +1,17 @@
+/*Program name: Big Or Small
+ *Name: Faith Uwaechi
+ Date: October 30th, 2024
+ Program Description: Write a program to check 2 parameters
+ */
+
+import static java.lang.System.*; 
+import java.util.Scanner;
+
+public class BigOrSmall
+{
+	public static void check( int a, int b )
+	{
+		
+		System.out.println(); //Answer
+	}
+}

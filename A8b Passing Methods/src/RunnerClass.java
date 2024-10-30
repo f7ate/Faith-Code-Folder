ff37431a168250b8d3f9d4e9b0765a5b05569536
f7ate <faith.uwaechi@rcsd.ca>
2024-10-30 09:17:00 -0600

    oct 30th

--- A8b Passing Methods/src/RunnerClass.java
+++ A8b Passing Methods/src/RunnerClass.java
@@ -1,5 +1,18 @@
-public class RunnerClass {
+/*Program name: Passing Methods
+ *Name: Faith Uwaechi
+ Date: October 30th, 2024
+ Program Description: Creating a sub class with 2 methods
+ */
+
+ //Runner Class
+ public class RunnerClass {
     public static void main(String[] args) {
         SubClass mySubClass = new SubClass();
+        //Print out gretting
+        System.out.println(mySubClass.getGreeting());
+
+        int number = 5;
+        // Run through number and return the result
+        System.out.println("The square of " + number + " is: " + mySubClass.squareNumber(number));
     }
 }
--- A8b Passing Methods/src/SubClass.java
+++ A8b Passing Methods/src/SubClass.java
@@ -1,13 +1,16 @@
 /*Program name: Passing Methods
  *Name: Faith Uwaechi
- Date: October 29th, 2024
+ Date: October 30th, 2024
  Program Description: Creating a sub class with 2 methods
  */
 
+ // SubClass
 public class SubClass {
+    //Method 1: Return a greeting/welcome message
     public String getGreeting() {
         return "Hello, welcome to this program";
     }
+    //Method 2: Calculate the square of a number
     public int squareNumber(int number) {
         return number * number;
     }

e8d0a9d610737431efbea6e6c0f9a24ac2896ec7
f7ate <faith.uwaechi@rcsd.ca>
2024-11-14 09:27:19 -0600

    nov 14th

---
+++ 1-Demos and Examples/MyFirstProject/src/PrimaryFile.java
@@ -0,0 +1,8 @@
+public class PrimaryFile {
+    public static void main(String[] args) throws Exception {
+        
+    }
+
+    for (int x = 0; x < 5; x++) {
+        for (int y = 0; y < 10; y++) {
+            System.out.print(x&y)

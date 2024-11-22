49965389e195f60de9899b1ce5be6c0b4b2f25a3
f7ate <faith.uwaechi@rcsd.ca>
2024-11-22 09:49:05 -0600

    nocv

---
+++ ArraysDemo1/src/App.java
@@ -0,0 +1,26 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // int[] scores = null;
+        // String[] names = null;
+
+        // Create or Initialize Array
+        // scores = new int[10];
+        // names = new String [10];
+
+        // Assign Values of Array
+        String[] names = {"a", "b", "c", "d", "e"};
+        int scores[] = {9000, 2342, 92384, 2048, 2096};
+
+        // Indiviual Positions
+        names[0] = "Wolf";
+        scores[0] = 9000;
+        
+        // Array Length Property
+        System.out.println("Scores length is: " + names.length);
+
+        // Outputting or Acesssing Arrays
+        
+        
+        System.out.println(scores[5]);
+    }
+}

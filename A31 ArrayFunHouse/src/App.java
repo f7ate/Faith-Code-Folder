d9ff5c49d34a21a6a4e528b77432ab726983d455
f7ate <faith.uwaechi@rcsd.ca>
2024-12-05 09:46:38 -0600

    dec 5th

--- A31 ArrayFunHouse/src/App.java
+++ A31 ArrayFunHouse/src/App.java
@@ -1,3 +1,10 @@
+/*Project Name: Array Fun House
+ Name: FAITH
+ Date: Dec 5th 2024
+ Description: Defining and outputing a variable.
+*/
+
+import java
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");

fee4d1ab4ac7c0e4beb2d836fa2228adca0a336e
f7ate <faith.uwaechi@rcsd.ca>
2025-01-19 22:28:12 -0600

    miles per hr

---
+++ A17 Miles per Hour/src/MilesPerHour.java
@@ -0,0 +1,13 @@
+/*Program name: Miles per Hour
+ *Name: Faith Uwaechi
+ Date: January 19th, 2025
+ Program Description: Given a distance traveled (miles) and elapsed time (hours and minutes), calculate the speed in miles per hour.  
+
+distance = rate * time          minutes / 60 = hours  ( 360 / 60 == 6 hours )
+ */
+
+public class MilesPerHour {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}

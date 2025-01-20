c18279fdffb241f9b9a1aad3016c14c47eac7574
f7ate <faith.uwaechi@rcsd.ca>
2025-01-19 18:58:54 -0600

    Start module one

---
+++ A13GeoCalc/src/Module.java
@@ -0,0 +1,20 @@
+/*Program name: Geometry Calculator
+ *Name: Faith Uwaechi
+ Date: January 19th, 2025
+ Program Description: You will be creating program that can complete the following calculations:
+
+Perimeter of a Rectangle. Formula: P = 2L + 2W or 2( L + W ) 
+Surface area of a  Cube.  Formula: SA = 6s2
+Area of a Circle. Formula: A = πr2 or A = Pi * r2
+ */
+
+
+public class Module {
+    public double calculateRectanglePerimeter(double length, double width) {
+        return 2 * (length + width);
+    }
+
+    public double calculateCubeSurfaceArea(double side) {
+        return 6 * Math.pow(side, 2);
+    }
+    }

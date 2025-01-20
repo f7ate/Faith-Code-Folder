dcd1bcf09139efcc83c8694b884d109ada449df3
f7ate <faith.uwaechi@rcsd.ca>
2025-01-19 19:56:41 -0600

    input for user

--- A13GeoCalc/src/ModuleRunner.java
+++ A13GeoCalc/src/ModuleRunner.java
@@ -8,8 +8,20 @@ Surface area of a  Cube.  Formula: SA = 6s2
 Area of a Circle. Formula: A = πr2 or A = Pi * r2
  */
 
+import java.util.Scanner;
 
 public class ModuleRunner {
+    public static void main(String[] args) {
+        Module module = new Module();
+        Scanner scanner = new Scanner(System.in);
     
+        System.out.println("Choose a calculation:");
+        System.out.println("1. Perimeter of a Rectangle");
+        System.out.println("2. Surface Area of a Cube");
+        System.out.println("3. Area of a Circle");
+        System.out.println("Enter your choice: 1, 2 or 3");
+        int choice = scanner.nextInt();
+
+    }
 
     }

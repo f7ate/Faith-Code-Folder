240860b396b5392d287e217a95474f4196e1729e
f7ate <faith.uwaechi@rcsd.ca>
2025-01-21 06:20:00 -0600

    Update App.java

--- A31 ArrayFunHouse/src/App.java
+++ A31 ArrayFunHouse/src/App.java
@@ -1,12 +1,21 @@
 /*Project Name: Array Fun House
- Name: FAITH
+ Name: Faith Uwaechi
  Date: Dec 5th 2024
- Description: Defining and outputing a variable.
+ Description:  Write several array manipulation methods.  One method will sum up a section of a provided array, another method will count up how many of a certain number occur in the array, and the last method will remove all of a certain value from the array.
+
 */
 
-import java
+import java.util.Arrays;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        int[] one = {4, 10, 0, 1, 7, 6, 5, 3, 2, 9};
+
+        //print out array
+        System.out.println("Array 1: " + Arrays.toString(one));
+
+        System.out.println("sum of spots 3-6 = " + getSum(one, 3, 6));
+        System.out.println("sum of spots 2-9 = " + getSum(one, 2, 9));
+
     }
 }

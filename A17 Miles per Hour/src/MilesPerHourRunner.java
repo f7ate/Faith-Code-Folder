f8ddab8d4b034cba64ff847fe2b70bd6303fab34
f7ate <faith.uwaechi@rcsd.ca>
2025-01-21 06:02:00 -0600

    inlcuded prompts

---
+++ A17 Miles per Hour/src/MilesPerHourRunner.java
@@ -0,0 +1,33 @@
+/*Program name: Miles per Hour
+ *Name: Faith Uwaechi
+ Date: January 19th, 2025
+ Program Description: Given a distance traveled (miles) and elapsed time (hours and minutes), calculate the speed in miles per hour.  
+
+distance = rate * time          minutes / 60 = hours  ( 360 / 60 == 6 hours )
+ */
+
+import java.util.Scanner;
+
+public class MilesPerHourRunner {
+    public static void main(String[] args) {
+        MilesPerHour mph = new MilesPerHour();
+        Scanner scanner = new Scanner(System.in);
+
+        //prompt for user
+        System.out.println("Enter the distance traveled in miles: ");
+        double distance = scanner.nextDouble();
+
+        System.out.println("Enter the time traveled in hours: ");
+        int hours = scanner.nextInt();
+        System.out.println("Enter the time traveled in minutes: ");
+        int minutes = scanner.nextInt();
+
+        //calculate the speed
+        double speed = mph.calculateSpeed(distance, hours, minutes);
+        //output 
+        System.out.println("The speed is: " + speed + " miles per hour.");
+
+        scanner.close();
+    
+    }
+}

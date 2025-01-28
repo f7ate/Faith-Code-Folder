12790a9df5bf170624e907c47a7e6d51911714ed
f7ate <faith.uwaechi@rcsd.ca>
2025-01-27 21:31:27 -0600

    including method to count cool numbers & loop

---
+++ A23 Cool Numbers/src/CoolNumbers.java
@@ -0,0 +1,25 @@
+/*Project Name: Cool Numbers
+ Name: Faith Uwaechi
+ Date: Jan 27th 2025
+ Description: Complete methods isCoolNum( n ) && countCoolNumbers( n ).  isCoolNum( n) will determine if a number is a cool number.  A cool number is any number that has a remainder of 1 when divided by 3,4,5, and 6.  countCoolNumbers will count the number
+of cool numbers between 6 and the parameter passed in.   
+*/
+
+
+public class CoolNumbers {
+    public static boolean isCoolNum (int n) {
+        return (n % 3 == 1) && (n % 4 == 1) && (n % 5 == 1) && (n % 6 == 1);
+    }
+
+    public static int countCoolNumbers(int max) {
+        int count = 0;
+
+        for (int i = 6; i < max; i++) {
+            if (isCoolNum(i)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}

2178ae4021da44b672049a48b286361918730cce
f7ate <faith.uwaechi@rcsd.ca>
2025-01-27 21:38:16 -0600

    Scanner input

---
+++ A23 Cool Numbers/src/CoolNumbersRunner.java
@@ -0,0 +1,14 @@
+/*Project Name: Cool Numbers
+ Name: Faith Uwaechi
+ Date: Jan 27th 2025
+ Description: Complete methods isCoolNum( n ) && countCoolNumbers( n ).  isCoolNum( n) will determine if a number is a cool number.  A cool number is any number that has a remainder of 1 when divided by 3,4,5, and 6.  countCoolNumbers will count the number
+of cool numbers between 6 and the parameter passed in.   
+*/
+
+import java.util.Scanner;
+
+public class CoolNumbersRunner {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}

ff8d062b4868b2fc4d7337395f7b54e2c94fd110
f7ate <faith.uwaechi@rcsd.ca>
2025-01-27 21:03:23 -0600

    runner class

---
+++ A22 Leap Year/src/LeapYearRunner.java
@@ -0,0 +1,13 @@
+/*Project Name: Leap Year
+ Name: Faith Uwaechi
+ Date: Jan 27th 2025
+ Description:  Write a program to determine if a year is a leap year.  A leap year is any year evenly divisible by 4 or 400.  You will need to use mod %.
+*/
+
+
+public class LeapYearRunner {
+    //To check if year is a leap year
+    public static boolean isLeapYear (int year) {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+}

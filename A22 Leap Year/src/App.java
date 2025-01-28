113e70149489a8d081422448c71978982f0d46d4
f7ate <faith.uwaechi@rcsd.ca>
2025-01-27 20:58:44 -0600

    leapyr

---
+++ A22 Leap Year/src/App.java
@@ -0,0 +1,12 @@
+/*Project Name: Leap Year
+ Name: Faith Uwaechi
+ Date: Jan 27th 2025
+ Description:  Write a program to determine if a year is a leap year.  A leap year is any year evenly divisible by 4 or 400.  You will need to use mod %.
+*/
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}

0c6f22f0610eacbb01b925fc451fd5ba7f19bfde
f7ate <faith.uwaechi@rcsd.ca>
2025-01-27 21:36:04 -0600

    loop

--- A23 Cool Numbers/src/CoolNumbers.java
+++ A23 Cool Numbers/src/CoolNumbers.java
@@ -11,9 +11,11 @@ public class CoolNumbers {
         return (n % 3 == 1) && (n % 4 == 1) && (n % 5 == 1) && (n % 6 == 1);
     }
 
+    //Method to count num of cool numbers between 6 & given num
     public static int countCoolNumbers(int max) {
         int count = 0;
 
+        //Loop
         for (int i = 6; i < max; i++) {
             if (isCoolNum(i)) {
                 count++;
